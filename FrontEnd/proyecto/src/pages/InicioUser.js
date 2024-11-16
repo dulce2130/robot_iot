1a78fb6f2e7aca3bce6fe63f8316af27b5139b49
Dulce <dulcevsgonsa@gmail.com>
2024-11-15 19:56:51 -0600

    Add saludo nuevo

--- FrontEnd/proyecto/src/pages/InicioUser.js
+++ FrontEnd/proyecto/src/pages/InicioUser.js
@@ -5,32 +5,31 @@ import typewriterEffect from '../funciones/funciones.js';
 import NavBar from "../components/NavBar.js";
 
 const Inicio = () => {
-  const { user } = useContext(AuthContext);
-  const nombre = user?.nombre;
+    const { user } = useContext(AuthContext);
+    const nombre = user?.nombre;
 
-  const getSaludo = () => {
-    const hora = new Date().getHours();
-    if (hora >= 6 && hora < 12) return "¡Buenos días";
-    if (hora >= 12 && hora < 18) return "¡Buenas tardes";
-    return "¡Buenas noches";
-  };
+    const getSaludo = () => {
+        const hora = new Date().getHours();
+        if (hora >= 6 && hora < 12) return "¡Buenos días";
+        if (hora >= 12 && hora < 18) return "¡Buenas tardes";
+        return "¡Buenas noches";
+    };
 
-  useEffect(() => {
-    const saludo = getSaludo();
-    typewriterEffect(nombre, saludo); 
-  }, [nombre]);
+    useEffect(() => {
+        const saludo = getSaludo();
+        typewriterEffect(nombre, saludo);
+    }, [nombre]);
 
-  return (
-    <>
-      <NavBar showMenu={true} />
+    return (
+        <>
+            <NavBar showMenu={true} />
 
-      <div className='container'>
-        <div className='txt-inicial'>
-          <h2 className="text-primary mb-4" id="typewriter"></h2>
-        </div>
-      </div>
-    </>
-  );
+
+            <div className='txt-inicial'>
+                <h2 className="text-primary mb-4" id="typewriter"></h2>
+            </div>
+        </>
+    );
 };
 
 export default Inicio;
